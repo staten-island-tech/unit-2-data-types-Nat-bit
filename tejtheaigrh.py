d33b5a0289e830c30619df518e7651cbbf146ad2
Nat-bit <144480946+Nat-bit@users.noreply.github.com>
2023-09-29 13:55:28 -0400

    hdgjhfkgh

--- tejtheaigrh.py
+++ tejtheaigrh.py
@@ -2,11 +2,11 @@
 x = input ("Eastbound?")
 y = input ("Westbound?")
 if x == "true" or "True" and y=="False" or "false":
-    x= True and print("There is east bound traffic.")
+    x== True and y== False
     if x== True and y== False:
-        print ("There is no west bound traffic")
+        print("There is east bound traffic.") and print ("There is no west bound traffic")
 elif y == "True"or "true" and x== "False" or "false":
-    y= True and print ("There is west bound traffic.")
+    y= True and x== False
     if y== True and x== False:
         print ("There is no east bound traffic.")
 else:
--- tipsacjagh.py
+++ tipsacjagh.py
@@ -1,13 +1,10 @@
+#Challenge
+##Create a function that accepts an input and determines all factors of the number.
+
+integer= int(input("Say a number"))
+print (integer)
+#first take inputs. check
+#Second somehow make a list that has the factors of such number in it. 
+#Third make a filter for that list to only print out the largest/highest/greatest factors?
 
 
-x = input ("Southbound?")
-y = input ("Westbound?")
-
-def traffic(x,y):
-    if x == y:
-         print ("false")
-    else :
-        print ("true")
-
-
-traffic (x,y)
